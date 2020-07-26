15151561a98c17d2cd9adb29bcaec73e942ba1df
kaique.motta@hotmail.com <xrlxrl157>
2020-07-26 19:07:45 -0300

    init

---
+++ src/main/java/kafka/deserializer/VendaDeserializer.java
@@ -0,0 +1,26 @@
+package kafka.deserializer;
+
+
+
+import java.io.IOException;
+
+import org.apache.kafka.common.serialization.Deserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kafka.model.Venda;
+
+public class VendaDeserializer implements Deserializer<Venda> {
+
+	@Override
+	public Venda deserialize(String topic, byte[] venda) {
+		try {
+			return new ObjectMapper().readValue(venda, Venda.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+}
